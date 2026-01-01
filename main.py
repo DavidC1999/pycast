e5a33e91ddadb008ad7683ceba50b9b50bca69b4
David <david@david.home>
2026-01-01 18:28:27 +0100

    Allow open controls without share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, current_app, request
 
-import subprocess
+import subprocess, os.path
 
 import x11 as platformspecific
 
@@ -25,24 +25,25 @@ def launch_youtube(id=None, time=None):
     
     launch_website(url)
 
+@app.route("/website")
 @app.route("/website/<path:url>")
 def route_launch_website(url=None):
-    if url is None:
-        return "<p>No URL given</p>"
-    
-    launch_website(url)
+    if url is not None:
+        launch_website(url)
+
     return render_template("website.html")
 
+@app.route("/youtube")
 @app.route("/youtube/<id>")
 def route_launch_youtube(id=None):
-    if id is None:
-        return "<p>No ID given</p>"
+    if id is not None:
+        launch_youtube(id)
 
-    launch_youtube(id)
-    return render_template("youtube.html", id=id)
+    return render_template("youtube.html", id=id or "")
 
-@app.route("/youtube/<id>/<action>")
-def route_youtube_simple_action(id=None, action=None):
+@app.route("/youtube_action/<action>")
+@app.route("/youtube_action/<action>/<id>")
+def route_youtube_action(action=None, id=None):
     if action == "toggleplay":
         platformspecific.keypress("k")
     elif action == "fullscreen":
@@ -59,19 +60,26 @@ def route_youtube_simple_action(id=None, action=None):
         platformspecific.keypress("F5")
     elif action == "captions":
         platformspecific.keypress("c")
-    elif action == "jump":
+    elif action == "jump" and id is not None:
         close_tab()
         minutes = request.args.get('m')
         seconds = request.args.get('s')
         launch_youtube(id, f"{minutes}m{seconds}s")
 
 
-    return render_template("youtube.html", id=id)
+    return render_template("youtube.html", id=id or "")
 
 @app.route("/close")
 def route_close():
     close_tab()
     return render_template("index.html")
+
+@app.route("/asset/<asset>")
+def route_asset(asset=None):
+    BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+    if os.path.exists(f"{BASE_DIR}/static/{asset}"):
+        return current_app.send_static_file(asset)
+    return current_app.send_static_file("notfound.png")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
