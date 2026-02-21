920f21082991ead48f30bbe61b9f6d752fd55160
David <david@david.home>
2026-02-21 15:31:47 +0100

    Add jump functionality to NPO using selenium

--- platforms/npo.py
+++ platforms/npo.py
@@ -1,7 +1,7 @@
 from pycast import *
 
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from selenium.webdriver.support.expected_conditions import presence_of_element_located, visibility_of_element_located
 from selenium.webdriver.common.by import By
 
 class Npo(Platform):
@@ -15,6 +15,7 @@ class Npo(Platform):
             (Action.forward, lambda: sendkey(Keys.ARROW_RIGHT)),
             (Action.refresh, lambda: session().driver.refresh()),
             (Action.captions, lambda: sendkey("c")),
+            (Action.jump, lambda: self._jump()),
         )
 
     def _open_youtube(self, id=None, time=None):
@@ -32,6 +33,49 @@ class Npo(Platform):
         # Focus the video player so keypesses can influence the playback
         session().driver.execute_script("document.getElementById(\"bitmovinplayer-video-null\").focus()")
 
+    def _jump(self):
+        minutes = int(get_url_arg('m'))
+        jump_seconds = int(get_url_arg('s')) + 60 * minutes
+
+        player_elem = session().driver.find_element(By.CLASS_NAME, "bitmovinplayer-container")
+        seek_bar_elem = session().driver.find_element(By.CSS_SELECTOR, ".bmpui-controlbar-seekbar .bmpui-seekbar")
+        total_time_elem = session().driver.find_element(By.CLASS_NAME, "bmpui-total-time")
+
+        text = total_time_elem.get_attribute("innerText")
+        parts = text.split(":")
+
+        video_length_s = 0
+        factor = 1
+        for part in reversed(parts):
+            video_length_s += int(part) * factor
+            factor *= 60
+        
+        element_width = seek_bar_elem.size["width"]
+        target_click = element_width * (jump_seconds / video_length_s) - element_width / 2
+
+        # Get the seek bar to show by moving the mouse:
+        action = webdriver.ActionChains(session().driver)
+        action.move_to_element(player_elem)
+        action.move_by_offset(10, 0)
+        action.perform()
+
+        # Wait until the seek bar is visible
+        WebDriverWait(session().driver, 10)\
+            .until(visibility_of_element_located((By.CSS_SELECTOR, ".bmpui-controlbar-seekbar .bmpui-seekbar")))
+
+        # Click at the correct location to jump to the specified time:
+        action = webdriver.ActionChains(session().driver)
+        action.move_to_element(seek_bar_elem)
+        action.move_by_offset(target_click, 0)
+        action.click()
+        action.perform()
+
+        # Move the mouse out of the way to the seekbar disappears again:
+        action = webdriver.ActionChains(session().driver)
+        action.move_to_element(player_elem)
+        action.perform()
+
+        
 
     def launch(self, params: dict[str, str]):
         open_browser(params["url"])
