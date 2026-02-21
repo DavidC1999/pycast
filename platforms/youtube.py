0f4c22b22dca38566859148aab06baa0f203d16f
David <david@david.home>
2026-02-21 16:21:37 +0100

    Allow sharing Youtube timestamp

--- platforms/youtube.py
+++ platforms/youtube.py
@@ -4,7 +4,7 @@ class Youtube(Platform):
     def __init__(self):
         super().__init__(
             "YouTube",
-            r".*(https?:\/\/(www\.)?)?youtube\.com\/watch\?v=(?P<id>[^?&#]+).*",
+            r".*?(?P<url>(https?:\/\/(www\.)?)?youtube\.com\/watch\?v=(?P<id>[^?&#]+).*)",
             (Action.toggleplay, lambda: sendkey("k")),
             (Action.fullscreen, lambda: sendkey("f")),
             (Action.bback, lambda: sendkey("j")),
@@ -16,25 +16,17 @@ class Youtube(Platform):
             (Action.jump, lambda: self._jump_action()),
         )
 
-    def _open_youtube(self, id=None, time=None):
-        url = f"https://www.youtube.com/watch?v={id}"
-
-        if time:
-            url += "&t=" + time
-        
-        open_browser(url)
-
     def _jump_action(self):
-        close_browser()
         minutes = get_url_arg('m')
         seconds = get_url_arg('s')
 
         id = session().user_args
-        self._open_youtube(id, f"{minutes}m{seconds}s")
+
+        session().driver.get(f"https://www.youtube.com/watch?v={id}&t={minutes}m{seconds}s")
 
     def launch(self, params: dict[str, str]):
         session().user_args = params["id"]
-        self._open_youtube(session().user_args)
+        open_browser(params["url"])
 
 def create():
     return Youtube()
